8ed82e74609627790e270589f6f6ce62f74e33ab
WuX <wux@thenest.dev>
2024-12-25 14:22:30 +0200

    feat(kern/x86_64): add cpuid

---
+++ kernel/src/arch/x86_64/sys/cpuid.c
@@ -0,0 +1,21 @@
+#include "cpuid.h"
+
+#include <cpuid.h>
+
+bool x86_64_cpuid_feature(x86_64_cpuid_feature_t feature) {
+    uint32_t value = 0;
+    if(x86_64_cpuid_register(feature.leaf, feature.reg, &value)) return false;
+    return (value & (1 << feature.bit));
+}
+
+bool x86_64_cpuid_register(uint32_t leaf, x86_64_cpuid_register_t reg, uint32_t *out) {
+    uint32_t eax = 0, ebx = 0, ecx = 0, edx = 0;
+    if(__get_cpuid(leaf, &eax, &ebx, &ecx, &edx) == 0) return true;
+    switch(reg) {
+        case X86_64_CPUID_REGISTER_EAX: *out = eax; break;
+        case X86_64_CPUID_REGISTER_EBX: *out = ebx; break;
+        case X86_64_CPUID_REGISTER_ECX: *out = ecx; break;
+        case X86_64_CPUID_REGISTER_EDX: *out = edx; break;
+    }
+    return false;
+}
