cb4b4a27b14f924b4f5a1c973f8a11dd5e8ba6d9
WuX <wux@thenest.dev>
2024-12-25 21:53:28 +0200

    feat(kern/arch): add header for info about page frames

---
+++ kernel/src/arch/page.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <stddef.h>
+
+#ifdef __ARCH_X86_64
+
+#define ARCH_PAGE_SIZE_4KB 0x1000
+#define ARCH_PAGE_SIZE_2MB 0x200000
+#define ARCH_PAGE_SIZE_1GB 0x40000000
+#define ARCH_PAGE_GRANULARITY ARCH_PAGE_SIZE_4KB
+
+#define ARCH_PAGE_SIZES ((size_t[]) { ARCH_PAGE_SIZE_4KB, ARCH_PAGE_SIZE_2MB, ARCH_PAGE_SIZE_1GB })
+#define ARCH_PAGE_SIZES_COUNT (sizeof(ARCH_PAGE_SIZES) / sizeof(size_t))
+
+#else
+#error Unimplemented
+#endif
+
+static_assert(ARCH_PAGE_GRANULARITY > 0);
+static_assert(ARCH_PAGE_SIZES_COUNT > 0);
