c2b0883106cf515874f5e979a877cac8419333e1
WuX <wux@thenest.dev>
2024-12-25 14:03:25 +0200

    feat(kern/mem): add hhdm

--- kernel/src/arch/x86_64/init.c
+++ kernel/src/arch/x86_64/init.c
@@ -1,5 +1,12 @@
 #include <tartarus.h>
+#include <stddef.h>
+
+uintptr_t g_hhdm_offset;
+size_t g_hhdm_size;
+
 
 [[noreturn]] void init([[maybe_unused]] tartarus_boot_info_t *boot_info) {
-    for(;;) asm volatile ("hlt");
+    g_hhdm_offset = boot_info->hhdm.offset;
+    g_hhdm_size = boot_info->hhdm.size;
+
 }
---
+++ kernel/src/memory/hhdm.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <stdint.h>
+#include <stddef.h>
+
+/**
+ * @brief Convert a physical address to a virtual HHDM address.
+ * @param ADDRESS physical address
+ * @returns virtual address within the HHDM
+ */
+#define HHDM(ADDRESS) ((uintptr_t) (ADDRESS) + g_hhdm_offset)
+
+/**
+ * @brief Convert a virtual address within the HHDM to a physical address.
+ * @param ADDRESS virtual address within the HHDM
+ * @returns physical address
+ */
+#define HHDM_TO_PHYS(ADDRESS) ((uintptr_t) (ADDRESS) - g_hhdm_offset)
+
+extern uintptr_t g_hhdm_offset;
+extern size_t g_hhdm_size; // TODO: Maybe do some bounds checking here. maybe under a debug flag?
