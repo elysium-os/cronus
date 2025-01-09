f40e2f703f06ea3e60be53d63fb3caa3d6e9d67a
WuX <wux@thenest.dev>
2025-01-09 12:40:43 +0200

    feat(kern/x86_64): add word-width port I/O

--- kernel/src/arch/x86_64/cpu/port.h
+++ kernel/src/arch/x86_64/cpu/port.h
@@ -21,15 +21,31 @@ static inline void x86_64_port_outb(uint16_t port, uint8_t value) {
 /**
  * @brief Port in (word).
  */
-static inline uint32_t x86_64_port_inl(uint16_t port) {
-    uint32_t result;
-    asm volatile("inl %1, %0" : "=a"(result) : "Nd"(port));
+static inline uint16_t x86_64_port_inw(uint16_t port) {
+    uint16_t result;
+    asm volatile("inw %1, %0" : "=a"(result) : "Nd"(port));
     return result;
 }
 
 /**
  * @brief Port out (word).
  */
-static inline void x86_64_port_outl(uint16_t port, uint32_t value) {
+static inline void x86_64_port_outw(uint16_t port, uint16_t value) {
+    asm volatile("outw %0, %1" : : "a"(value), "Nd"(port));
+}
+
+/**
+ * @brief Port in (dword).
+ */
+static inline uint32_t x86_64_port_ind(uint16_t port) {
+    uint32_t result;
+    asm volatile("inl %1, %0" : "=a"(result) : "Nd"(port));
+    return result;
+}
+
+/**
+ * @brief Port out (dword).
+ */
+static inline void x86_64_port_outd(uint16_t port, uint32_t value) {
     asm volatile("outl %0, %1" : : "a"(value), "Nd"(port));
 }
