99386f5709cf39d5107535734f5a4d1c90cd2b20
WuX <wux@thenest.dev>
2025-02-01 03:34:38 +0200

    chore: update tools

---
+++ tools/dump.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+ADDRESS=0x$1
+
+objdump .chariot-cache/target/kernel/install/usr/share/kernel.elf \
+    -d -wrC \
+    --visualize-jumps=color \
+    --disassembler-color=on \
+    --start-address=$ADDRESS \
+    --stop-address=0x$(printf '%x' $(($ADDRESS + 128)))
---
+++ tools/gen_kernel_headers.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+chariot target/tartarus source/uacpi
