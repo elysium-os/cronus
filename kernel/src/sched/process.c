23c5e415fe8a2e1c745de7644de03062e2b711f5
WuX <wux@thenest.dev>
2025-03-24 21:47:55 +0200

    fix: add missing process_create

---
+++ kernel/src/sched/process.c
@@ -0,0 +1,22 @@
+#include "process.h"
+
+#include "memory/heap.h"
+
+static long g_next_pid = 1;
+
+static spinlock_t g_sched_processes_lock = SPINLOCK_INIT;
+list_t g_sched_processes = LIST_INIT;
+
+process_t *process_create(vm_address_space_t *address_space) {
+    process_t *proc = heap_alloc(sizeof(process_t));
+    proc->id = __atomic_fetch_add(&g_next_pid, 1, __ATOMIC_RELAXED);
+    proc->lock = SPINLOCK_INIT;
+    proc->threads = LIST_INIT;
+    proc->address_space = address_space;
+
+    interrupt_state_t previous_state = spinlock_acquire(&g_sched_processes_lock);
+    list_append(&g_sched_processes, &proc->list_sched);
+    spinlock_release(&g_sched_processes_lock, previous_state);
+
+    return proc;
+}
