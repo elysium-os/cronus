036a639220074bc2bdb0d30cfc55a83dd174389b
WuX <wux@thenest.dev>
2025-03-25 21:13:37 +0200

    refactor: clean up list implementation

--- kernel/lib/list.c
+++ kernel/lib/list.c
@@ -1,24 +1,26 @@
 #include "list.h"
 
+#include <stddef.h>
+
 void list_append(list_element_t *position, list_element_t *element) {
     element->prev = position;
     element->next = position->next;
-    if(position->next) position->next->prev = element;
+    if(position->next != NULL) position->next->prev = element;
     position->next = element;
 }
 
 void list_prepend(list_element_t *position, list_element_t *element) {
     element->next = position;
     element->prev = position->prev;
-    if(position->prev) position->prev->next = element;
+    if(position->prev != NULL) position->prev->next = element;
     position->prev = element;
 }
 
 void list_delete(list_element_t *element) {
-    if(element->prev) element->prev->next = element->next;
-    if(element->next) element->next->prev = element->prev;
+    if(element->prev != NULL) element->prev->next = element->next;
+    if(element->next != NULL) element->next->prev = element->prev;
 }
 
 bool list_is_empty(list_t *list) {
-    return !list->next || list == list->next;
+    return list->next == NULL || list->next == list;
 }
