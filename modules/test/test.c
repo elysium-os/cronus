8df9afa1944cf7cf6068b0ad940a3b0f157e19b1
WuX <wux@thenest.dev>
2025-04-30 08:33:24 +0300

    feat: compile test module with headers

--- modules/test/test.c
+++ modules/test/test.c
@@ -1,13 +1,5 @@
-typedef enum {
-    LOG_LEVEL_ERROR,
-    LOG_LEVEL_WARN,
-    LOG_LEVEL_INFO,
-    LOG_LEVEL_DEBUG,
-    LOG_LEVEL_DEVONLY
-} log_level_t;
+#include "common/log.h"
 
-[[gnu::format(printf, 3, 4)]] void log(log_level_t level, const char *tag, const char *fmt, ...);
-
-void module_init() {
+void __module_initialize() {
     log(LOG_LEVEL_INFO, "TEST_MODULE", "Hello world");
 }
